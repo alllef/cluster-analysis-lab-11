d010fb822148c72c6d5f3129964e6de322cbd978
alllef <alekz29@gmail.com>
2021-05-20 21:47:35 +0300

    added method to normalize data

--- src/main/java/com/github/alllef/datautils/DataUtility.java
+++ src/main/java/com/github/alllef/datautils/DataUtility.java
@@ -3,30 +3,40 @@ package com.github.alllef.datautils;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class DataUtility {
 
-    public static List<double[]> readNumData(File fileToRead) {
-        List<double[]> data = new ArrayList<>();
+    private double[][]data;
+
+DataUtility(File fileToRead){
+    readNumData(fileToRead);
+}
+
+
+    public double[][] readNumData(File fileToRead) {
+        List<double[]> dataList = new ArrayList<>();
 
         try (FileReader fileReader = new FileReader(fileToRead)) {
             Scanner scan = new Scanner(fileReader);
             scan.nextLine();
 
             while (scan.hasNextLine())
-                data.add(getRowValues(scan.nextLine()));
+                dataList.add(getRowValues(scan.nextLine()));
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+         data = new double[dataList.size()][];
+
+        for (int i = 0; i < data.length; i++)
+            data[i] = dataList.get(i);
+
         return data;
     }
 
-    private static double[] getRowValues(String row) {
+    private double[] getRowValues(String row) {
 
         String[] strValues = row.split(",");
         double[] numValues = new double[strValues.length];
@@ -37,8 +47,27 @@ public class DataUtility {
         return numValues;
     }
 
-    public static void main(String[]args){
-        List<double[]> data = DataUtility.readNumData(new File("wine-clustering.csv"));
+    public void normalizeData() {
+
+
+        Map<Integer, Double> minColumnValues = new HashMap<>();
+        Map<Integer, Double> maxColumnValues = new HashMap<>();
+
+
+        for (int j = 0; j < data[0].length; j++) {
+            for (int i = 0; i < data.length; i++) {
+                if (minColumnValues.get(j) == null || minColumnValues.get(j) > data[i][j])
+                    minColumnValues.put(j, data[i][j]);
+                if (maxColumnValues.get(j) == null || maxColumnValues.get(j) < data[i][j])
+                    maxColumnValues.put(j, data[i][j]);
+            }
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++)
+                data[i][j] = (data[i][j] - minColumnValues.get(j)) / (maxColumnValues.get(j) - minColumnValues.get(j));
+        }
+
     }
 
 }
